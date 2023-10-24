f12c7603ea2bf734c7bf939ea76022cc7099a4e3
Ken Luzuriaga <ken.luzuriaga@trescloud.com>
2023-10-24 16:31:44 -0500

    update oca modules

--- stock_no_negative/__manifest__.py
+++ stock_no_negative/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Stock Disallow Negative",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.2",
     "category": "Inventory, Logistic, Storage",
     "license": "AGPL-3",
     "summary": "Disallow negative stock levels by default",
@@ -14,5 +14,4 @@
     "depends": ["stock"],
     "data": ["views/product_product_views.xml", "views/stock_location_views.xml"],
     "installable": True,
-    "auto_install": True,
 }
